4fda77dad28ce027b326c3467b0729e232fde854
tom n <xcapement@proton.me>
2025-04-27 08:51:58 -0500

    first successful build of home-manager, one bug

--- flake.nix
+++ flake.nix
@@ -3,18 +3,31 @@
 
   inputs = {
    nixpkgs.url = "nixpkgs/nixos-24.11";
+   home-manager.url = "github:nix-community/home-manager/release-24.11";
+   home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, nixpkgs, ...}:
+  outputs = {self, nixpkgs, home-manager, ...}:
   let
+    system = "x86_64-linux";
     lib = nixpkgs.lib;
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
   nixosConfigurations = {
-    vrust = lib.nixosSystem {
-      system = "x86_64-linux";
-      odules = [
-        ./configuration.nix
-      ];
+      vrust = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  homeConfigurations = {
+      tom = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+        ];
+      };
     };
   };
 }
