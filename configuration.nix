0e3e14e7ee512df684fd208a09ac4cbfd4a3e81c
tom n <xcapement@proton.me>
2025-04-27 07:19:57 -0500

    enabled flakes

--- configuration.nix
+++ configuration.nix
@@ -136,4 +136,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
+  # install flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
